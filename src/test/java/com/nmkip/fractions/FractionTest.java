0dcf7c0b79d3045863d53f2d437d63a5eb16f644
nmkip <juan.ignacio848@gmail.com>
2018-08-27 22:19:34 -0300

    fraction project

---
+++ src/test/java/com/nmkip/fractions/FractionTest.java
@@ -0,0 +1,12 @@
+package com.nmkip.fractions;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class FractionTest {
+
+    @Test
+    public void nothing() {
+    }
+}
