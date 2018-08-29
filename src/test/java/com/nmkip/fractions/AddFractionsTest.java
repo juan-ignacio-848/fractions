c15da738715354bb9b047804c2d38e5273de9cec
nmkip <juan.ignacio848@gmail.com>
2018-08-28 23:50:55 -0300

    We now add non-zero, non-negative integers.

--- src/test/java/com/nmkip/fractions/AddFractionsTest.java
+++ src/test/java/com/nmkip/fractions/AddFractionsTest.java
@@ -23,4 +23,10 @@ public class AddFractionsTest {
         final Fraction sum = new Fraction(0).plus(new Fraction(5));
         assertEquals(5, sum.intValue());
     }
+
+    @Test
+    public void nonNegativeNonZeroOperands() {
+        final Fraction sum = new Fraction(3).plus(new Fraction(4));
+        assertEquals(7, sum.intValue());
+    }
 }
