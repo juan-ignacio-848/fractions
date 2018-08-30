7a2255a746f8dce2ee84551e5765780c76180d4b
nmkip <juan.ignacio848@gmail.com>
2018-08-30 20:40:56 -0300

    We no longer fake the denominator.

--- src/main/java/com/nmkip/fractions/Fraction.java
+++ src/main/java/com/nmkip/fractions/Fraction.java
@@ -1,17 +1,21 @@
 package com.nmkip.fractions;
 
 public class Fraction {
-    private int integerValue;
+    private final int denominator;
+    private final int integerValue;
 
     public Fraction(int integerValue) {
         this.integerValue = integerValue;
+        this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
+        this.integerValue = numerator;
+        this.denominator = denominator;
     }
 
     public Fraction plus(Fraction that) {
-        return new Fraction(this.integerValue + that.integerValue);
+        return new Fraction(this.integerValue + that.integerValue, denominator);
     }
 
     public int intValue() {
@@ -23,6 +27,6 @@ public class Fraction {
     }
 
     public int getDenominator() {
-        return 5;
+        return denominator;
     }
 }
--- src/test/java/com/nmkip/fractions/AddFractionsTest.java
+++ src/test/java/com/nmkip/fractions/AddFractionsTest.java
@@ -33,7 +33,7 @@ public class AddFractionsTest {
 
     @Test
     public void nonTrivialButCommonDenominator() {
-        final Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 3));
+        final Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
         assertEquals(3, sum.getNumerator());
         assertEquals(5, sum.getDenominator());
     }
