7c952a887a8c40e18cd3d529b3481e15908218f7
nmkip <juan.ignacio848@gmail.com>
2018-08-30 23:41:29 -0300

    Defined gcd(0,0) = 1

--- src/main/java/com/nmkip/fractions/NumberTheory.java
+++ src/main/java/com/nmkip/fractions/NumberTheory.java
@@ -3,6 +3,10 @@ package com.nmkip.fractions;
 public class NumberTheory {
 
     public static int gcd(int a, int b) {
+        if(a == 0 && b == 0) {
+            return 1;
+        }
+
         while (b != 0) {
             int t = b;
             b = a % t;
