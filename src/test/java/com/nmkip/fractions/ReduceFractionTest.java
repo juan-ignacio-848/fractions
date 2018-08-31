6702f7490ba019db6aed2f29ba57940114dc77ae
nmkip <juan.ignacio848@gmail.com>
2018-08-30 23:14:09 -0300

    We now reduce a Fraction in the constructor.

--- src/test/java/com/nmkip/fractions/ReduceFractionTest.java
+++ src/test/java/com/nmkip/fractions/ReduceFractionTest.java
@@ -1,6 +1,5 @@
 package com.nmkip.fractions;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -13,7 +12,6 @@ public class ReduceFractionTest {
     }
 
     @Test
-    @Ignore("Waiting to implement GCD")
     public void reduceToNotWholeNumber() {
         assertEquals(new Fraction(3,4), new Fraction(6, 8));
     }
