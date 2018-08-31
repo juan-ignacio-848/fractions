9e04f58d01a24fad9423286a5e1634b78491c472
nmkip <juan.ignacio848@gmail.com>
2018-08-30 21:05:46 -0300

    I feel confident enough that equals() works, except for reducing.

--- src/test/java/com/nmkip/fractions/FractionEqualsTest.java
+++ src/test/java/com/nmkip/fractions/FractionEqualsTest.java
@@ -3,11 +3,32 @@ package com.nmkip.fractions;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotSame;
 
 public class FractionEqualsTest {
 
     @Test
     public void sameNumeratorAndDenominator() {
         assertEquals(new Fraction(3, 5), new Fraction(3, 5));
+    }
+
+    @Test
+    public void differentNumerators() {
+        assertNotSame(new Fraction(1,5), new Fraction(2,5));
+    }
+
+    @Test
+    public void differentDenominators() {
+        assertNotSame(new Fraction(1,5), new Fraction(2,7));
+    }
+
+    @Test
+    public void wholeNumberEqualsSameFraction() {
+        assertEquals(new Fraction(5, 1), new Fraction(5));
+    }
+
+    @Test
+    public void wholeNUmberNotEqualToDifferentWholeNumber() {
+        assertNotSame(new Fraction(6), new Fraction(5));
     }
 }
