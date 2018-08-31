80a71e7b3f7f5dcfd74840e996724905e656c521
nmkip <juan.ignacio848@gmail.com>
2018-08-30 22:34:10 -0300

    We can add fractions with different denominators, as long as we don't have to reduce the results.

--- src/test/java/com/nmkip/fractions/AddFractionsTest.java
+++ src/test/java/com/nmkip/fractions/AddFractionsTest.java
@@ -36,5 +36,8 @@ public class AddFractionsTest {
         assertEquals(new Fraction(3,5), new Fraction(1, 5).plus(new Fraction(2, 5)));
     }
 
-
+    @Test
+    public void differentDenominatorsWithoutReducing() {
+        assertEquals(new Fraction(5,6), new Fraction(1,2).plus(new Fraction(1, 3)));
+    }
 }
