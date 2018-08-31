7b69931275c83b88dd7f1d15c6ea71e16d918720
nmkip <juan.ignacio848@gmail.com>
2018-08-30 23:31:56 -0300

    Fraction now shifts negative sign away from the denominator.

--- src/test/java/com/nmkip/fractions/FractionEqualsTest.java
+++ src/test/java/com/nmkip/fractions/FractionEqualsTest.java
@@ -31,4 +31,10 @@ public class FractionEqualsTest {
     public void wholeNUmberNotEqualToDifferentWholeNumber() {
         assertNotSame(new Fraction(6), new Fraction(5));
     }
+
+    @Test
+    public void negativeDenominator() {
+        assertEquals(new Fraction(1, 2), new Fraction(-1, -2));
+        assertEquals(new Fraction(-1, 2), new Fraction(1, -2));
+    }
 }
